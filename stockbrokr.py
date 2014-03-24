31aedff2ff63933fc77fce2d384134a42f74d168
Lisa Gress <lisamgress@gmail.com>
2014-03-23 21:25:29 -0700

    Hash passwords using bcrypt

--- stockbrokr.py
+++ stockbrokr.py
@@ -1,4 +1,5 @@
 import os
+import bcrypt
 from flask import Flask, render_template, request, session, flash, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -15,7 +16,7 @@ app.config.update(dict(
 class User(db.Model):
 	user_id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(150), unique=True)
-	password = db.Column(db.String(30))
+	password = db.Column(db.String(255))
 	first_name = db.Column(db.String(50))
 	last_name = db.Column(db.String(80))
 	balance = db.Column(db.Integer)
@@ -23,7 +24,7 @@ class User(db.Model):
 
 	def __init__(self, email, password):
 		self.email = email
-		self.password = password
+		self.password = bcrypt.hashpw(password, bcrypt.gensalt())
 
 class Stock(db.Model):
 	owner_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
@@ -42,7 +43,9 @@ def login():
 	if request.method == 'POST':
 		user = User.query.filter_by(email=request.form['email']).first()
 		if user:
-			if user.password == request.form['password']:
+			password_entered = request.form['password'].encode('UTF8')
+			password_stored = user.password.encode('UTF8')
+			if bcrypt.hashpw(password_entered, password_stored) == password_stored:
 				session['logged_in'] = user.user_id
 				flash("You were logged in")
 				return redirect(url_for('index'))
