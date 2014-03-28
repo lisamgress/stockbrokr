b8393c26ceb2fb7898ae5c5943a60995091bd758
Lisa Gress <lisamgress@gmail.com>
2014-03-27 17:56:29 -0700

    Update current price of the stock in the portfolio and implement ability to sell shares
    When a user goes to their portfolio, the current price of the stock is updated through a call to to Yahoo Finance.  Currently only the user's stocks are updated but a later implementation may explore updating all the stocks in the Stock table at a regular interval (every 15 min?).
    
    Implemented ability to sell shares.  User can sell shares from the portfolio page.  Still need to delete stocks from user's portfolio if the user has zero shares.
    
    Updated flash messages to use a category and deleted any instances of passing error messages to templates.
    
    Centered text in td elements.

--- stockbrokr.py
+++ stockbrokr.py
@@ -4,6 +4,9 @@ from passlib.hash import pbkdf2_sha512
 from flask import Flask, render_template, request, session, flash, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 
+STOCK_API_URL = 'http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=sl1d1t1c1ohgv&e=.csv'
+STARTING_BALANCE = 10000.00
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "postgresql://stockbrokr@localhost/stockbrokr")
@@ -28,7 +31,7 @@ class User(db.Model):
         self.password = pbkdf2_sha512.encrypt(password)
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = 10000.00
+        self.balance = STARTING_BALANCE
 
 class Stock(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
@@ -62,6 +65,12 @@ def get_current_user():
         return None
     return User.query.get(session['logged_in'])
 
+def get_stock_info(symbol):
+    url = STOCK_API_URL % (symbol)
+    r = requests.get(url)
+    data = csv.reader(StringIO(r.text))
+    row = get_first_row(data)
+    return row
 
 @app.route('/')
 def index():
@@ -69,7 +78,9 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
+    user = get_current_user()
+    if user:
+        return redirect(url_for('index'))
     if request.method == 'POST':
         user = User.query.filter_by(email=request.form['email']).first()
         if user:
@@ -77,31 +88,33 @@ def login():
             password_stored = user.password.encode('UTF8')
             if pbkdf2_sha512.verify(password_entered, password_stored):
                 session['logged_in'] = user.user_id
-                flash(user.first_name + ", you were logged in")
+                flash("%s, you were logged in" % (user.first_name), 'alert-success')
                 return redirect(url_for('index'))
             else:
-                error = 'Invalid password'
+                flash("Invalid password", 'alert-danger')
+                return redirect(url_for('login'))
         else:
-            error = "Email not found.  Please create an account"
-            return render_template('register.html', error=error)
-    return render_template('login.html', error=error)
+            flash("Email not found.  Please create an account", 'alert-warning')
+            return redirect(url_for('register'))
+    return render_template('login.html')
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
-    flash('You were logged out')
+    flash('You were logged out', 'alert-success')
     return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    error = None
     if request.method == 'POST':
         user = User.query.filter_by(email=request.form['email']).first()
         if user:
-            error = "That email address is already associated with an account"
+            flash("That email address is already associated with an account. Please log in.", 'alert-warning')
+            return redirect(url_for('login'))
         else:
             if request.form['password'] != request.form['confirm_password']:
-                error = "Confirmed password must match password"
+                flash("Confirmed password must match password", 'alert-danger')
+                return redirect(url_for('register'))
             else:
                 new_user = User(request.form['email'],
                                 request.form['password'],
@@ -110,16 +123,20 @@ def register():
                 db.session.add(new_user)
                 db.session.commit()
                 session['logged_in'] = new_user.user_id
-                flash(new_user.first_name + ", thanks for registering")
+                flash("%s, thanks for registering" % (new_user.first_name ), 'alert-success')
                 return redirect(url_for('index'))
-    return render_template('register.html', error=error)
+    return render_template('register.html')
 
 @app.route('/portfolio')
 def portfolio():
     user = get_current_user()
     if not user:
-        flash("You must log in to view your portfolio")
+        flash("You must log in to view your portfolio", 'alert-info')
         return redirect(url_for('login'))
+    for stock in user.portfolio:
+        row = get_stock_info(stock.symbol)
+        stock.current_price = row[1]
+        db.session.commit()
     return render_template('portfolio.html', user=user)
 
 @app.route('/lookup_stock', methods=['GET', 'POST'])
@@ -128,14 +145,10 @@ def lookup_stock():
     stock_info = {}
     user = get_current_user()
     if not user:
-        flash("You must log in to look up stocks")
+        flash("You must log in to look up stocks", 'alert-info')
         return redirect(url_for('login'))
     if request.method == 'POST':
-        symbol = request.form['symbol']
-        url = 'http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=sl1d1t1c1ohgv&e=.csv' % (symbol)
-        r = requests.get(url)
-        data = csv.reader(StringIO(r.text))
-        row = get_first_row(data)
+        row = get_stock_info(request.form['symbol'])
         stock_info = {
             'symbol' : row[0],
             'current' : float(row[1]),
@@ -154,13 +167,13 @@ def lookup_stock():
 def buy_stock():
     user = get_current_user()
     if not user:
-        flash("You must log in to buy shares")
+        flash("You must log in to buy shares", 'alert-info')
         return redirect(url_for('login'))
-    if request.method == 'POST':
+    if request.method == 'POST':        
         symbol = request.form['symbol']
         already_own = Stock.query.filter_by(owner_id=user.user_id, symbol=symbol).first()
         if already_own:
-            flash("You already own that stock")
+            flash("You already own that stock", 'alert-info')
             return redirect(url_for('portfolio'))
         shares = request.form['shares']
         share_purchase_price = request.form['current']
@@ -170,11 +183,40 @@ def buy_stock():
             db.session.add(new_stock)
             user.balance -= total_purchase_price
             db.session.commit()
-            flash("You purchased %s share(s) of %s." % (shares, symbol))
+            flash("You purchased %s share(s) of %s." % (shares, symbol), 'alert-success')
             return redirect(url_for('portfolio'))
         else:
-            flash("You don't have enough BrokrBucks to buy that many shares")
+            flash("You don't have enough BrokrBucks to buy that many shares", 'alert-warning')
     return redirect(url_for('lookup_stock'))
+
+@app.route('/sell_stock/<symbol>')
+def sell_stock(symbol):
+    user = get_current_user()
+    stock_info = None
+    if not user:
+        flash("You must log in to buy shares", 'alert-info')
+        return redirect(url_for('login'))
+    stock_info = Stock.query.filter_by(owner_id=user.user_id, symbol=symbol).first()
+    return render_template('sell.html', stock_info=stock_info)
+
+
+@app.route('/process_sale/<symbol>', methods=['GET', 'POST'])
+def process_sale(symbol):
+    user = get_current_user()
+    if not user:
+        flash("You must log in to buy shares", 'alert-info')
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        stock_info = Stock.query.filter_by(owner_id=user.user_id, symbol=symbol).first()
+        shares_for_sale = int(request.form['shares'])
+        if shares_for_sale > stock_info.shares:
+            flash("You don't have that many shares to sell.", 'alert-danger')
+            return redirect(url_for('sell_stock', symbol=symbol))
+        user.balance += shares_for_sale * stock_info.current_price
+        stock_info.shares -= shares_for_sale
+        db.session.commit()
+        flash("You sold %s share(s) of %s" % (shares_for_sale, symbol), 'alert-success')
+    return redirect(url_for('portfolio'))
 
 
 
